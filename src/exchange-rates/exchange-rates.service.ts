64a3bb10e2e114382204e560a8ffccc3b6692b6b
Unknownplanet40 <ryanjamesc4@gmail.com>
2025-01-11 04:44:03 +0800

    historical

--- src/exchange-rates/exchange-rates.service.ts
+++ src/exchange-rates/exchange-rates.service.ts
@@ -57,6 +57,53 @@ export class ExchangeRatesService {
             </body>
             </html>
         `;
+  }
 
+  async getHistoricalRates(date: string, base: string, symbols: string): Promise<any> {
+    const url = `${this.baseUrl}/${date}`;
+    const params = {
+      access_key: this.configService.get<string>('EXCHANGERATES_API_KEY'),
+      base,
+      symbols,
+    };
+
+    const response = this.httpService.get(url, { params });
+    const result = await lastValueFrom(response).then(res => res.data);
+    return `
+            <html>
+            <head>
+                <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    background-color:rgba(245, 245, 245, 0.5);
+                }
+                .container {
+                    max-width: 800px;
+                    margin: 0 auto;
+                    padding: 20px;
+                    background-color: #fff;
+                }
+                </style>
+            </head>
+            <body>
+                <div class="container">
+                    <h1>Exchange Rates for ${new Date(date).toLocaleDateString()}</h1>
+                    <p>Date: ${new Date(date).toLocaleDateString()}</p>
+                    <p>Currency: ${base}</p>
+                    <p>Rates for:</p>
+                    <ul>
+                        ${Object.keys(result.rates)
+                          .map(
+                            key =>
+                              `<li>${key}: ${result.rates[key].toFixed(2)}</li>`,
+                          )
+                          .join('')}
+                    </ul>
+                </div>
+            </body>
+            </html>
+        `;
   }
 }
